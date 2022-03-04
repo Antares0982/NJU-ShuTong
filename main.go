62aa220f414a4394ce7baa2a7d4602cb7820dcfa
antares0982@gmail.com <Antares0982@gmail.com>
2022-03-04 21:41:48 +0800

    :new: 检测充值

--- main.go
+++ main.go
@@ -14,12 +14,29 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 )
 
 var EAI_SESS = os.Getenv("EAI_SESS")
 var ROOM = strings.ReplaceAll(os.Getenv("ROOM"), " ", "")
 var TGID = os.Getenv("TGID")
 var TGBOTID = os.Getenv("TGBOTID")
+var tofile = os.Getenv("TOFILE")
+
+func getFloatFromFile(filename string) (float64, error) {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return 0, err
+	}
+
+	s := string(bs)
+	f, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return f, nil
+}
 
 func sendMessage(content string) (string, error) {
 	url := "https://api.telegram.org/bot" + TGBOTID + "/sendMessage?chat_id=" + TGID + "&text=" + content
@@ -60,48 +77,77 @@ func doGet(url string, headers map[string]string) (string, error) {
 }
 
 func getElectricBalance(areaId string, buildId string, roomId string) (float64, error) {
-	url := fmt.Sprintf("https://wx.nju.edu.cn/njucharge/wap/electric/charge?area_id=%s&build_id=%s&room_id=%s", areaId, buildId, roomId)
-	body, err := doGet(url, map[string]string{
-		"Cookie": "eai-sess=" + EAI_SESS,
-	})
-	if err != nil {
-		return 0, err
-	}
-	reg := regexp.MustCompile(`dianyue\w*:\w*"([\d\.]+)"`)
-	result := reg.FindStringSubmatch(body)
+	for i := 0; i < 5; i++ {
+		url := fmt.Sprintf("https://wx.nju.edu.cn/njucharge/wap/electric/charge?area_id=%s&build_id=%s&room_id=%s", areaId, buildId, roomId)
 
-	if len(result) != 2 {
-		return 0, errors.New("EAI_SESS 失效了")
+		body, err := doGet(url, map[string]string{
+			"Cookie": "eai-sess=" + EAI_SESS,
+		})
+
+		if err != nil {
+			if i == 4 {
+				return 0, err
+			}
+			time.Sleep(5 * time.Second)
+			continue
+		}
+
+		reg := regexp.MustCompile(`dianyue\w*:\w*"([\d\.]+)"`)
+		result := reg.FindStringSubmatch(body)
+
+		if len(result) != 2 {
+			if i == 4 {
+				return 0, errors.New("EAI_SESS 失效了")
+			}
+			time.Sleep(5 * time.Second)
+			continue
+		}
+
+		return strconv.ParseFloat(result[1], 64)
 	}
-	return strconv.ParseFloat(result[1], 64)
+	panic(errors.New("unable to reach here"))
 }
 
 func getElectricInfo() {
 	ids := strings.SplitN(ROOM, ",", 3)
 
 	if len(ids) != 3 {
-		sendMessage("寝室未配置或配置错误，或未配置 EAI_SESS，不开启寝室电量监测")
+		sendMessage("寝室未配置或配置错误，或未配置 EAI_SESS")
 		panic(errors.New("寝室未配置或配置错误"))
 	}
 
 	electric, err := getElectricBalance(ids[0], ids[1], ids[2])
 
+	if err != nil {
+		sendMessage("获取寝室电量出错了：" + err.Error())
+		panic(err)
+	}
+
 	log.Println("electric left:", electric, err)
 
-	var info string
+	if electric <= 15 {
+		sendMessage("寝室电量不足 15 度啦～")
+	}
+
+	old_electric, err := getFloatFromFile(tofile)
 	if err != nil {
-		info, err = sendMessage("获取寝室电量出错了：" + err.Error())
-	} else if electric <= 15 {
-		info, err = sendMessage("寝室电量不足 15 度啦～")
+		sendMessage("从文件读取电量出错了：" + err.Error())
+		panic(err)
 	}
 
-	// do nothing if electric > 15
-
-	if err == nil {
-		if info != "" {
-			log.Println(info)
-		}
+	if old_electric < electric {
+		money := int((electric - old_electric) * 0.58)
+		sendMessage("充值成功！上次记录的电量：" + strconv.FormatFloat(old_electric, 'E', -1, 64) + "，现在电量：" + strconv.FormatFloat(electric, 'E', -1, 64) + "，可能的充值金额大约为" + strconv.Itoa(money))
 	}
+
+	err = ioutil.WriteFile(tofile, []byte(strconv.FormatFloat(electric, 'E', -1, 64)), 0644)
+
+	if err != nil {
+		sendMessage("写入文件出错了：" + err.Error())
+		panic(err)
+	}
+
+	log.Println("Program successfully executed")
 }
 
 func main() {
